a5e025e5baa367c7a9a01fdeda227f13193e17b1
William Mosto <will.mosto@gmail.com>
2021-07-26 14:41:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name : 'Will',
+    streetAddress : '385 Union ave'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeObject = {...employee};
+    newEmployeeObject[key]=value;
+    return newEmployeeObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee = employee;
+    delete newEmployee.name;
+    return newEmployee;
+}
